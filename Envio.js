c7e9413e528fe49ae9a639867c7d94e4ad9be8db
Isenpa <83470340+Isenpa@users.noreply.github.com>
2022-03-17 16:23:50 -0500

    Update Envio.js

--- Envio.js
+++ Envio.js
@@ -29,7 +29,7 @@ $form.addEventListener('submit', (event) => {
  // let fecha = document.querySelector("#fecha").value;
  // let hora = document.querySelector("#hora").value;
  // let empleado = document.querySelector("#empleado").value;
-  let price = document.querySelector("#price").innerText;
+  let price = document.querySelector('#price').innerText
 //  let servicio = document.querySelector("#servicio").value;
 //  let total = document.querySelector("#total").innerHTML;
 
